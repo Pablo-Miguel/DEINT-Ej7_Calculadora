2390b8f729ffd2b402f5a48064d5e5fd9feff678
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-01-24 14:43:51 +0100

    Agregar archivos de proyecto.

---
+++ DEINT-Ej7_Calculadora/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DEINT_Ej7_Calculadora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DEINT-Ej7_Calculadora/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DEINT_Ej7_Calculadora;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
